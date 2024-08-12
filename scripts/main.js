ddb5009686fc4df929420b8b40f4a1c7fee50e1f
jacob3015 <jacob3015@gmail.com>
2024-08-12 22:28:09 +0900

    add templates for layouts, favicons, styles for layouts, scripts to load layouts

---
+++ scripts/main.js
@@ -0,0 +1,87 @@
+const baseUrl = window.location.origin;
+
+class App {
+
+    constructor() {
+        this.layouts = {
+            header: document.createElement("header"),
+            menu: document.createElement("menu"),
+            contents: Object.assign(document.createElement("div"), {
+                id: "contents"
+            }),
+            footer: document.createElement("footer")
+        };
+        this.parser = new DOMParser();
+    }
+
+    loadDefaultPage() {
+        this.loadLayouts();
+        this.loadHeader();
+        this.loadFooter();
+        this.loadMenu();
+    }
+
+    loadLayouts() {
+        const topContainer = Object.assign(document.createElement("div"), {
+            id: "top-container"
+        });
+        topContainer.append(this.layouts.header, this.layouts.menu);
+
+        document.body.append(topContainer, this.layouts.contents, this.layouts.footer);
+    }
+
+    async loadHeader() {
+        try {
+            const response = await fetch(`${baseUrl}/templates/header.html`);
+            if (!response.ok) {
+                throw new Error("http error");
+            }
+            const htmlText = await response.text();
+
+            const document = this.parser.parseFromString(htmlText, "text/html");
+
+            const template = document.querySelector("template");
+            const content = document.importNode(template.content, true);
+            this.layouts.header.append(content);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    async loadFooter() {
+        try {
+            const response = await fetch(`${baseUrl}/templates/footer.html`);
+            if (!response.ok) {
+                throw new Error("http error");
+            }
+            const htmlText = await response.text();
+            
+            const document = this.parser.parseFromString(htmlText, "text/html");
+            const template = document.querySelector("template");
+            const content = document.importNode(template.content, true);
+            this.layouts.footer.append(content);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    async loadMenu() {
+        try {
+            const response = await fetch(`${baseUrl}/templates/menu.html`);
+            if (!response.ok) {
+                throw new Error("http error");
+            }
+            const htmlText = await response.text();
+            
+            const document = this.parser.parseFromString(htmlText, "text/html");
+            const template = document.querySelector("template");
+            const content = document.importNode(template.content, true);
+            this.layouts.menu.append(content);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+}
+
+const app = new App();
+app.loadDefaultPage();
